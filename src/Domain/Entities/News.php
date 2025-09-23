a294741b3835f4e2dab18c28cb37801f44e6b272
Álvaro Valencia <alvaro.vamen@gmail.com>
2025-09-23 17:38:48 +0200

    setup local mysql database and Doctrine. First migration to create News table based in News entity

--- src/Domain/Entities/News.php
+++ src/Domain/Entities/News.php
@@ -2,15 +2,38 @@
 
 namespace App\Domain\Entities;
 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'news')]
 class News
 {
-    public function __construct(
-        private string $title,
-        private string $url,
-        private string $summary,
-        private string $source
-    ) {}
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
 
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    private string $url;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $title;
+
+    #[ORM\Column(type: 'text')]
+    private string $summary;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private string $source;
+
+    public function __construct(string $title, string $url, string $summary, string $source)
+    {
+        $this->title = $title;
+        $this->url = $url;
+        $this->summary = $summary;
+        $this->source = $source;
+    }
+
+    public function getId(): ?int { return $this->id; }
     public function getTitle(): string { return $this->title; }
     public function getUrl(): string { return $this->url; }
     public function getSummary(): string { return $this->summary; }
